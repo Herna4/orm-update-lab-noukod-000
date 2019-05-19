cb87b2facc2d036067bbe264b4047af3f3c9df3d
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-19 20:32:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,36 +53,6 @@ class Student
     student.save
     student
   end
-  
-  # def self.new_from_db(row)
-  #   # create a new Student object given a row from the database
-  #   student = self.new(row[1], row[2], row[0])
-  #   student
-  # end
-  
-  # def self.find_by_name(name)
-  #   # find the student in the database given a name
-  #   # return a new instance of the Student class
-  #   sql = <<-SQL
-  #     SELECT * FROM students
-  #     WHERE name = ?
-  #     LIMIT 1
-  #     SQL
-
-  #   DB[:conn].execute(sql, name).map do |row|
-  #     self.new_from_db(row)
-  #   end.first
-  # end
-  
-  # def update
-  #   sql = <<-SQL
-  #     UPDATE students
-  #     SET name = ?, grade = ?
-  #     WHERE id = ?
-  #     SQL
-
-  #     DB[:conn].execute(sql, self.name, self.grade, self.id)
-  # end
     
   def self.new_from_db(row)
     id = row[0]
