e5e786b5da90bc0ec281590d66b9a840bebb8114
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-19 20:03:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,24 +17,14 @@ class Student
   
   def self.create_table
     sql =  <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS Students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        album TEXT
+        grade TEXT
         )
         SQL
     DB[:conn].execute(sql)
   end
-  def self.create_table 
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS Students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-      )
-    SQL 
-    
-    DB[:]
-    
-  end
+  
+  
 end
