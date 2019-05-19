b5c2ef71039b6f6691d6315838c366724eeaaba3
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-19 20:23:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,6 +48,14 @@ class Student
     end
   end
   
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    student = self.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
   
   def self.find_by_name(name)
     # find the student in the database given a name
